547a4187f0a95e93d3cf862fd16cfef7e57ef7a4
Henning Halfpap <hhalfpap@mac.com>
2017-12-28 17:06:35 +0100

    First dummy views added

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MealWeek
 
 # Create your views here.
 def mealweek_list(request):
-    return render(request, 'mealplanner/mealweek_list.html')
+    mealweeks = MealWeek.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'mealplanner/mealweek_list.html', {'mealweeks': mealweeks})
