70cb2046a0437b00c5cb2b123910ec5a26d885f2
Henning Halfpap <hhalfpap@mac.com>
2018-01-01 15:10:40 +0100

    Refactor home page view to use a template

--- core/tests.py
+++ core/tests.py
@@ -1,20 +1,10 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 
 from core.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.strip().startswith('<html>'))
-        self.assertIn('<title>Halfpap Family Server</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+    def test_uses_core_index_template(self):
+        response = self.client.get('/')  
+        self.assertTemplateUsed(response, 'core/index.html')
