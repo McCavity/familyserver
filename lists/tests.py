e8527f99dd6ba38e3259440d277050d6b8443a5d
Henning Halfpap <hhalfpap@mac.com>
2018-01-01 16:27:57 +0100

    Functional tests assert that data gets sent

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_uses_lists_index_template(self):
+    def test_uses_lists_home_template(self):
         response = self.client.get('/lists/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
