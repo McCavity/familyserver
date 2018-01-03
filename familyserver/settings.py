ece9d3f95cfeda11f05a7775454cb67a925b5179
Henning Halfpap <hhalfpap@mac.com>
2018-01-03 09:37:10 +0100

    set STATIC_ROOT in settings and disable admin

--- familyserver/settings.py
+++ familyserver/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'udtapp01', '192.168.178.65'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
