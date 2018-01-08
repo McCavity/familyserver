d2b1e24219ea5a058724238f7e273e81b7f59938
Henning Halfpap <henning.halfpap@factset.com>
2018-01-08 13:11:45 +0100

    FT & qunit tests, core.js with click listeners

--- core/static/js/core.js
+++ core/static/js/core.js
@@ -1,5 +1,9 @@
-var initialize = function () {
+window.Familyserver = {};
+window.Familyserver.initialize = function () {
   $('input[name="text"]').on('keypress', function () {
+    $('.has-error').hide();
+  });
+  $('input[name="text"]').on('click', function () {
     $('.has-error').hide();
   });
 };
