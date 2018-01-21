23665c6b21ce72fe689ec4a40a4bf1591e298711
Henning Halfpap <hhalfpap@mac.com>
2018-01-21 13:54:52 +0100

    Added logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from accounts import views
+from django.contrib.auth.views import logout
 
 app_name = 'accounts'
 app_verbose_name = 'User Management'
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send_login_email'),
     path('login', views.login, name='login'),
+    path('logout', logout, {'next_page': '/'}, name='logout'),
 ]
